d173bdde442992686c478b99b4ed1c20b8e1a7fd
cpeterson384 <christianpeterson441769@gmail.com>
2022-08-25 20:54:44 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Christian Peterson
+ *         cpeterson61@unl.edu
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Christian Peterson\n");
+
+  return 0;
+}
